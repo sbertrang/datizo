1308f1a264faa99ba127badc5bfa4af18d95f326
Simon Bertrang <janus@errornet.de>
2012-08-05 17:38:35 +0200

    add dependencies and definitions

---
+++ lib/strtoi.c
@@ -0,0 +1,21 @@
+
+#include <limits.h>
+#include <stdlib.h>
+
+/*
+ * strtoi --- just like strtol, but returns int not long
+ */
+int
+strtoi(const char *nptr, char **endptr, int base)
+{
+	long		val;
+
+	val = strtol(nptr, endptr, base);
+#ifdef HAVE_LONG_INT_64
+	if (val != (long) ((int32) val))
+		errno = ERANGE;
+#endif
+	return (int) val;
+}
+
+
