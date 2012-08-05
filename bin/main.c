ceb541ca461b6e6a74a95b338194ec781936c0b2
Simon Bertrang <janus@errornet.de>
2012-08-05 16:57:10 +0200

    add timestamptz type

--- bin/main.c
+++ bin/main.c
@@ -2,9 +2,13 @@
 #include <err.h>
 #include <sysexits.h>
 
+#include "datizo.h"
+
 int
 main(int argc, char *argv [])
 {
+	TimestampTz	tsz;
+
 	warnx("test...");
 
 	return EX_OK;
---
+++ lib/datizo.h
@@ -0,0 +1,9 @@
+#ifndef	__DATIZO_H__
+#define	__DATIZO_H__
+
+#include <stdint.h>
+
+typedef	int64_t	TimestampTz;
+
+
+#endif	/* __DATIZO_H__ */
