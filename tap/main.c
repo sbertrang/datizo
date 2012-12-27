8dcdd70b6dd724eb804af5e22c8de868d9240ca3
Simon Bertrang <janus@errornet.de>
2012-12-27 15:10:23 +0100

    test output in different timezones

--- tap/main.c
+++ tap/main.c
@@ -21,7 +21,7 @@ main(int argc, char *argv [])
 
 	diag("test...");
 
-	rc = plan_tests(4);
+	rc = plan_tests(10);
 
 	session_timezone = pg_tzset("Europe/Amsterdam");
 	rc = ok( session_timezone != NULL, "session timezone defined" );
@@ -32,6 +32,20 @@ main(int argc, char *argv [])
 	s = timestamptz_out(tsz);
 	rc = ok( s != NULL, "timestamp as string: %s", s );
 	rc = ok( strcmp( s, "2011-12-31 20:20:01+01" ) == 0, "timestamp matches format" );
+
+	session_timezone = pg_tzset("Canada/Mountain");
+	rc = ok( session_timezone != NULL, "switch to canada timezone" );
+	s = timestamptz_out(tsz);
+	rc = ok( s != NULL, "timestamp as string: %s", s );
+	rc = ok( strcmp( s, "2011-12-31 12:20:01-07" ) == 0, "timestamp in canada" );
+
+
+	session_timezone = pg_tzset("Indian/Maldives");
+	rc = ok( session_timezone != NULL, "switch to maldives timezone" );
+	s = timestamptz_out(tsz);
+	rc = ok( s != NULL, "timestamp as string: %s", s );
+	rc = ok( strcmp( s, "2012-01-01 00:20:01+05" ) == 0, "timestamp in indian maldives" );
+	
 
 	return exit_status();
 }
