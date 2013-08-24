f11663cef326ccd620382e8bcc92ee292076e3f8
Simon Bertrang <janus@errornet.de>
2013-08-24 15:46:51 +0200

    adjust pod and readme at least a little bit

--- perl/Datizo/lib/Datizo.pm
+++ perl/Datizo/lib/Datizo.pm
@@ -47,49 +47,38 @@ __END__
 
 =head1 NAME
 
-Datizo - Perl extension for blah blah blah
+Datizo - Perl extension for the Datizo library to handle Date, Time and Timezones
 
 =head1 SYNOPSIS
 
   use Datizo;
-  blah blah blah
+  ...
 
 =head1 DESCRIPTION
 
-Stub documentation for Datizo, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
+XS binding for the Datizo library.
 
 =head2 EXPORT
 
 None by default.
 
-
-
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
+DateTime on CPAN
 
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+L<https://github.com/sbertrang/datizo>
 
 =head1 AUTHOR
 
-A. U. Thor, E<lt>simon@E<gt>
+Simon Bertrang, E<lt>janus@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2012 by A. U. Thor
+Copyright (C) 2012 by Simon Bertrang
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.12.2 or,
 at your option, any later version of Perl 5 you may have available.
 
-
 =cut
+
